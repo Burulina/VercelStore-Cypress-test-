bfa55f2d4583e120493322bf8ac1c0acafd63d70
Volodymyr <vaburulina@gmail.com>
2022-10-12 10:20:45 +0300

    Improved stability of tests

--- cypress/e2e/AddProductsToCart.cy.js
+++ cypress/e2e/AddProductsToCart.cy.js
@@ -9,7 +9,6 @@ const firstName = faker.name.firstName();
 const lastName = faker.name.lastName();
 const address = faker.address.street();
 const city = faker.address.cityName();
-const postalCode = faker.address.zipCode('#####');
 
 //Products for add to cart according test assingment
 const products = [{name: "ACME Cup", quantToAdd: 5}, {name: "Quarter Zip", quantToAdd: 1}];
@@ -19,7 +18,8 @@ describe('E-commerce VercelStore testing', () => {
     cy.visit('https://demo.vercel.store/');
     cy.url().should('contain', 'demo.vercel.store/');
     productsPage.getNewArrivalsNavbarLink().click();
-    productsPage.getProductName(products[0].name).click();
+    cy.url().should('contain', 'demo.vercel.store/search/clothes');
+    productsPage.getProductName(products[0].name).should('be.visible').click();
     productsPage.getProductPrice().as('product1Price');
     productsPage.getAddToCartButton().click();
     cartPage.increaseProductQuantitiy(products[0].quantToAdd);
@@ -29,7 +29,8 @@ describe('E-commerce VercelStore testing', () => {
     })
     cartPage.getCloseCartButton().click();
     productsPage.getFeaturedNavbarLink().click();
-    productsPage.getProductName(products[1].name).click();
+    cy.url().should('contain', 'demo.vercel.store/search/featured');
+    productsPage.getProductName(products[1].name).should('be.visible').click();
     productsPage.getSizeMButton().click();
     productsPage.getProductPrice().as('product2Price');
     productsPage.getAddToCartButton().click();
@@ -45,8 +46,8 @@ describe('E-commerce VercelStore testing', () => {
     })
     cartPage.getProceedCheckoutButton().click();
     // here is no way to import a function into cy.origin, so this code without using pageObjects 
-    cy.origin('https://next-js-store.myshopify.com/', { args: { email, firstName, lastName, address, city, postalCode } }, 
-    ({ email, firstName, lastName, address, city, postalCode }) => {
+    cy.origin('https://next-js-store.myshopify.com/', { args: { email, firstName, lastName, address, city } }, 
+    ({ email, firstName, lastName, address, city }) => {
     cy.url().should('contain', 'next-js-store.myshopify.com/');
     cy.get('#main-header').should('be.visible').and('contain.text', 'Contact information');
     cy.get('#checkout_email_or_phone').type(email);
@@ -54,10 +55,10 @@ describe('E-commerce VercelStore testing', () => {
     cy.get('#checkout_shipping_address_last_name').type(lastName);
     cy.get('#checkout_shipping_address_address1').type(address);
     cy.get('#checkout_shipping_address_city').type(city);
-    cy.get('#checkout_shipping_address_zip').type(postalCode);
+    cy.get('#checkout_shipping_address_zip').type('21012');
     cy.get('#continue_button').click();
     cy.get('#main-header').should('be.visible').and('contain.text', 'Shipping method');
-    cy.get('#checkout_shipping_rate_id_usps-prioritymailinternational-58_26').check();
+    cy.get('#checkout_shipping_rate_id_usps-prioritymailinternational-58_26', {timeout: 10000}).check();
     cy.get('#continue_button').click();
     cy.get('#main-header').should('be.visible').and('contain.text', 'Payment');
     cy.get('.section--payment-method p').should('be.visible').and('contain.text', 'This store can’t accept payments right now.');
